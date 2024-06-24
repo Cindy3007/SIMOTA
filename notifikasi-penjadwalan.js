558ddf2fb0fd444e7d632be8935bf1560afc8147
Cindy3007 <144988858+Cindy3007@users.noreply.github.com>
2024-06-24 16:47:43 +0700

    Add files via upload

---
+++ notifikasi-penjadwalan.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const port = 3000;
+
+// Middleware to serve static files
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Route to serve the HTML file
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'notifikasi-penjadwalan.html'));
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}/notifikasi-penjadwalan.html`);
+});
